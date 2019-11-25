5556476563dcdc9f865e89338d8b2f76cc1b326f
feliser <bonopoape@gmail.com>
2019-11-25 10:50:17 -0500

    Update AcronymService

--- src/main/java/com/feliser/services/AcronymService.java
+++ src/main/java/com/feliser/services/AcronymService.java
@@ -1,20 +1,23 @@
 package com.feliser.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.feliser.models.AcronymRequestModel;
 import com.feliser.models.AcronymResponseModel;
 
 @Service
-public class AcronymService 
-{
-	public AcronymResponseModel createAcronym(AcronymRequestModel acronymRequest)
-	{
-		for (int i = 0; i < acronymRequest.request.size(); i++)
-		{
+public class AcronymService {
+	public AcronymResponseModel createAcronym(AcronymRequestModel acronymRequest) {
+		return new AcronymResponseModel(generateAcronyms(acronymRequest));
+	}
+
+	public List<String> generateAcronyms(AcronymRequestModel acronymRequest) {
+		for (int i = 0; i < acronymRequest.request.size(); i++) {
 			acronymRequest.request.set(i, acronymRequest.request.get(i) + "s");
 		}
 		
-		return new AcronymResponseModel(acronymRequest.request);
+		return acronymRequest.request;
 	}
 }
