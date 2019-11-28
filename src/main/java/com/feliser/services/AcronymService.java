775ebbe27bd65437a4e2072e2b99cf0112172caf
feliser <bonopoape@gmail.com>
2019-11-28 00:52:00 -0500

    Update AcronymService.java

--- src/main/java/com/feliser/services/AcronymService.java
+++ src/main/java/com/feliser/services/AcronymService.java
@@ -1,5 +1,9 @@
 package com.feliser.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -14,10 +18,34 @@ public class AcronymService {
 	}
 
 	public List<String> generateAcronyms(AcronymRequestModel acronymRequest) {
-		for (int i = 0; i < acronymRequest.request.size(); i++) {
-			acronymRequest.request.set(i, acronymRequest.request.get(i) + "s");
-		}
+		List<String> acronyms = new ArrayList<String>();
 		
-		return acronymRequest.request;
+		//Get words list
+		BufferedReader br = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("words/corncob_caps.txt")));
+		
+		String word;
+		int beginIndex;
+		try {
+			while ((word = br.readLine()) != null) {
+				//Checks if characters for word appear in order in query
+				beginIndex = -1;
+				for(int i = 0; i < word.length(); i++)
+				{
+					System.out.println(word + " " + acronymRequest.request.get(0) + " " + beginIndex + " " + i);
+					if((beginIndex = (acronymRequest.request.get(0).toUpperCase().indexOf(word.charAt(i), beginIndex + 1))) == -1)
+					{
+						break;
+					}
+					if(i == word.length() - 1)
+					{
+						acronyms.add(word);
+					}
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return acronyms;
 	}
 }
