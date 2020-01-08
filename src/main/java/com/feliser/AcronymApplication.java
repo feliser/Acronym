ab67e3c39568693798727ebcb3f290dedeb57904
feliser <bonopoape@gmail.com>
2020-01-08 11:21:39 -0500

    Update index.html

--- src/main/java/com/feliser/AcronymApplication.java
+++ src/main/java/com/feliser/AcronymApplication.java
@@ -1,10 +1,17 @@
 package com.feliser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AcronymApplication {
+public class AcronymApplication extends SpringBootServletInitializer {
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AcronymApplication.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(AcronymApplication.class, args);
 	}
